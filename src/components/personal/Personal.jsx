810bc824dec94256337cb42181c9969cdc84c7ae
Palma <angelonovpalma29@gmail.com>
2024-11-11 05:46:58 -0600

    Se agrego la funcionalidad de exel

--- src/components/personal/Personal.jsx
+++ src/components/personal/Personal.jsx
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { Navbar } from '../Navbar.jsx';
 import { useNavigate } from "react-router-dom";
 import { reportarEntrada } from '../../services/api.jsx';
+import * as XLSX from 'xlsx'; // Importa la librería xlsx
 import './personal.css';
 import defaultAvatar from '../../assets/img/palmamorro.jpg';
 import earlyImage from '../../assets/img/comprobado.png';
 import lateImage from '../../assets/img/cerca.png';
 
 export const Personal = () => {
- const user = JSON.parse(localStorage.getItem('datosUsuario')) || {};
+  const user = JSON.parse(localStorage.getItem('datosUsuario')) || {};
   const [attendanceRecords, setAttendanceRecords] = useState([]);
   const navigate = useNavigate();
   const userId = user.account?.homeAccountId || "Invitado";
@@ -24,21 +25,24 @@ export const Personal = () => {
     const currentTime = new Date().toTimeString().split(' ')[0];
     const status = new Date().getHours() < 8 ? "A tiempo" : "Tarde";
 
-    const record = {
-      user: userName,
-      date: todayDate,
-      time: currentTime,
-      status,
-    };
-
     try {
-     
+      const ipResponse = await fetch('https://api.ipify.org?format=json');
+      const ipData = await ipResponse.json();
+      const userIp = ipData && ipData.ip ? ipData.ip : 'IP no disponible';
+
+      const record = {
+        user: userName,
+        date: todayDate,
+        time: currentTime,
+        status,
+        ip: userIp,
+      };
+
       const response = await reportarEntrada(record);
 
-      // Maneja la respuesta de la API
       if (response.error) {
-        console.error(response.e);
-        alert("Error al registrar la asistencia: " + response.e.message);
+        console.error(response.error);
+        alert("Error al registrar la asistencia: " + response.error.message);
       } else {
         const updatedRecords = [...attendanceRecords, record];
         setAttendanceRecords(updatedRecords);
@@ -52,6 +56,22 @@ export const Personal = () => {
     }
   };
 
+  const exportToExcel = () => {
+    const todayDate = new Date().toISOString().split('T')[0]; // Obtén la fecha de hoy en formato YYYY-MM-DD
+  
+    // Filtra los registros que tengan la fecha de hoy
+    const todayRecords = attendanceRecords.filter(record => {
+      const recordDate = new Date(record.date).toISOString().split('T')[0];
+      return recordDate === todayDate;
+    });
+  
+    // Crea el archivo Excel solo con los registros del día actual
+    const worksheet = XLSX.utils.json_to_sheet(todayRecords);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Reporte de Asistencia");
+    XLSX.writeFile(workbook, `Reporte_Asistencia_${todayDate}.xlsx`);
+  };
+
   const [formState, setFormState] = useState({
     todayDate: new Date().toISOString().split('T')[0],
     currentTime: new Date().toTimeString().split(' ')[0],
@@ -62,16 +82,12 @@ export const Personal = () => {
   });
 
   const usuarioLogueado = JSON.parse(localStorage.getItem('datosUsuario')) || {};
-
   const currentHour = new Date().getHours();
   const isOnTime = currentHour < 8 ? "A tiempo" : "Tarde";
   const imageToShow = currentHour < 8 ? earlyImage : lateImage;
   const backgroundColor = currentHour < 8 ? '#359100' : '#8b0000';
-
-  // Colores para las ondas
   const waveColors = currentHour < 8 ? ['#030e2e', '#023a0e', '#05a00d'] : ['#8b0000', '#b22222', '#ff4500'];
 
-  // Obtener la hora inicial desde internet solo una vez
   const fetchInternetTime = async () => {
     try {
       const response = await fetch('http://worldtimeapi.org/api/timezone/America/Guatemala');
@@ -87,11 +103,9 @@ export const Personal = () => {
     }
   };
 
-  // Actualizar la hora en el cliente cada segundo
   useEffect(() => {
-    fetchInternetTime(); // Llama a la API solo una vez para obtener la hora inicial
+    fetchInternetTime();
     const interval = setInterval(() => {
-      // Aumentar los segundos en el estado local
       setFormState((prevState) => {
         const updatedTime = new Date();
         updatedTime.setSeconds(updatedTime.getSeconds() + 1);
@@ -102,7 +116,6 @@ export const Personal = () => {
       });
     }, 1000);
 
-    // Limpieza del intervalo al desmontar el componente
     return () => clearInterval(interval);
   }, []);
 
@@ -167,6 +180,9 @@ export const Personal = () => {
                   d="M25 35.5C24.1716 35.5 23.5 36.1716 23.5 37C23.5 37.8284 24.1716 38.5 25 38.5V35.5ZM49.0607 38.0607C49.6464 37.4749 49.6464 36.5251 49.0607 35.9393L39.5147 26.3934C38.9289 25.8076 37.9792 25.8076 37.3934 26.3934C36.8076 26.9792 36.8076 27.9289 37.3934 28.5147L45.8787 37L37.3934 45.4853C36.8076 46.0711 36.8076 47.0208 37.3934 47.6066C37.9792 48.1924 38.9289 48.1924 39.5147 47.6066L49.0607 38.0607ZM25 38.5L48 38.5V35.5L25 35.5V38.5Z"
                 ></path>
               </svg>
+            </button>
+            <button onClick={exportToExcel} className="export-btn">
+              Exportar a Excel
             </button>
           </div>
         ) : (

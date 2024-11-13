8aaa3dc488cb638b8fb89a062a50d1458888e582
Palma <angelonovpalma29@gmail.com>
2024-11-12 19:44:35 -0600

    Prueba 7

--- src/components/personal/Personal.jsx
+++ src/components/personal/Personal.jsx
@@ -27,7 +27,7 @@ export const Personal = () => {
     const currentHour = new Date().getHours();
 
     // Permitir registrar asistencia solo entre las 7 y las 10 a.m. si no se ha registrado ya hoy
-    const isWithinAllowedTime = currentHour >= 7 && currentHour <= 17;
+    const isWithinAllowedTime = currentHour >= 19 && currentHour < 20;
     setIsButtonDisabled(lastAttendanceDate === formState.todayDate || !isWithinAllowedTime);
   }, [formState.todayDate, userId]);
 
@@ -79,12 +79,6 @@ export const Personal = () => {
     }
   };
 
-  const currentHour = new Date().getHours();
-  const isOnTime = currentHour < 8 ? "A tiempo" : "Tarde";
-  const imageToShow = currentHour < 8 ? earlyImage : lateImage;
-  const backgroundColor = currentHour < 8 ? '#359100' : '#8b0000';
-  const waveColors = currentHour < 8 ? ['#030e2e', '#023a0e', '#05a00d'] : ['#8b0000', '#b22222', '#ff4500'];
-
   const fetchInternetTime = async () => {
     try {
       const response = await fetch('http://worldtimeapi.org/api/timezone/America/Guatemala');
@@ -133,12 +127,7 @@ export const Personal = () => {
       <div className="posts-personal">
         {user ? (
           <div className="e-card playing">
-            <div className="image"></div>
-            <div className="wave" style={{ background: `linear-gradient(744deg, ${waveColors[0]}, ${waveColors[1]} 60%, ${waveColors[2]})` }}></div>
-            <div className="wave" style={{ background: `linear-gradient(744deg, ${waveColors[0]}, ${waveColors[1]} 60%, ${waveColors[2]})`, top: '210px' }}></div>
-            <div className="wave" style={{ background: `linear-gradient(744deg, ${waveColors[0]}, ${waveColors[1]} 60%, ${waveColors[2]})`, top: '420px' }}></div>
-
-            <div className='content-user'>
+            <div className="content-user">
               <div className="infotop">
                 <img
                   src={user.profilePicture || defaultAvatar}
@@ -150,62 +139,29 @@ export const Personal = () => {
                   <div className="user-department">{user.account.username}</div>
                 </div>
               </div>
-              <div className="date-time">
-                <div className="card" style={{ background: backgroundColor }}>
-                  <div className="card-content">
-                    <div className="card-top">
-                      <p>{isOnTime}</p>
-                    </div>
-                    <div className="card-bottom">
-                      <p>{formState.todayDate}</p>
-                      <p>{formState.currentTime}</p>
-                    </div>
-                  </div>
-                  <div className="card-image">
-                    <img
-                      src={imageToShow}
-                      alt="Status Icon"
-                      className="status-icon"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-            <button onClick={handleShowPopup} disabled={isButtonDisabled}>
-              <span>Enviar</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 74 74"
-                height="34"
-                width="34"
-              >
-                <circle strokeWidth="3" stroke="black" r="35.5" cy="37" cx="37"></circle>
-                <path
-                  fill="black"
-                  d="M25 35.5C24.1716 35.5 23.5 36.1716 23.5 37C23.5 37.8284 24.1716 38.5 25 38.5V35.5ZM49.0607 38.0607C49.6464 37.4749 49.6464 36.5251 49.0607 35.9393L39.5147 26.3934C38.9289 25.8076 37.9792 25.8076 37.3934 26.3934C36.8076 26.9792 36.8076 27.9289 37.3934 28.5147L45.8787 37L37.3934 45.4853C36.8076 46.0711 36.8076 47.0208 37.3934 47.6066C37.9792 48.1924 38.9289 48.1924 39.5147 47.6066L49.0607 38.0607ZM25 38.5L48 38.5V35.5L25 35.5V38.5Z"
-                ></path>
-              </svg>
-            </button>
+              <button onClick={handleShowPopup} disabled={isButtonDisabled}>
+                <span>Enviar</span>
+              </button>
 
-            {showPopup && (
-              <div className="popup">
-                <div className="popup-content">
-                  <p>¿Estás seguro de que deseas registrar tu asistencia?</p>
-                  <textarea
-                    placeholder="Escribe aquí la razón de tu asistencia"
-                    value={reason}
-                    onChange={(e) => setReason(e.target.value)}
-                    rows="4"
-                    style={{ width: '100%', marginTop: '10px', padding: '8px' }}
-                  />
-                  <div className="popup-actions">
-                    <button onClick={handleConfirmAttendance}>Sí</button>
-                    <button onClick={handleCancelAttendance}>No</button>
+              {showPopup && (
+                <div className="popup">
+                  <div className="popup-content">
+                    <p>¿Estás seguro de que deseas registrar tu asistencia?</p>
+                    <textarea
+                      placeholder="Escribe aquí la razón de tu asistencia"
+                      value={reason}
+                      onChange={(e) => setReason(e.target.value)}
+                      rows="4"
+                      style={{ width: '100%', marginTop: '10px', padding: '8px' }}
+                    />
+                    <div className="popup-actions">
+                      <button onClick={handleConfirmAttendance}>Sí</button>
+                      <button onClick={handleCancelAttendance}>No</button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            )}
+              )}
+            </div>
           </div>
         ) : (
           <p>No hay datos del usuario disponibles.</p>

42ba0050b07b903b753f17c24b4161c15ece3a8f
Palma <angelonovpalma29@gmail.com>
2024-11-23 22:08:39 -0600

    ya no doy 95

--- src/components/personal/Personal.jsx
+++ src/components/personal/Personal.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Navbar } from '../Navbar.jsx';
-import { Header } from "../header/Header.jsx"; 
+import { reportarEntrada } from '../../services/api.jsx';
+import { Header } from "../header/Header.jsx"; // Importamos el Header
 import './personal.css';
 import defaultAvatar from '../../assets/img/palmamorro.jpg';
+import earlyImage from '../../assets/img/comprobado.png';
+import lateImage from '../../assets/img/cerca.png';
 
 export const Personal = () => {
   const user = JSON.parse(localStorage.getItem('datosUsuario')) || {};
@@ -17,35 +20,19 @@ export const Personal = () => {
 
   const [showPopup, setShowPopup] = useState(false);
   const [reason, setReason] = useState("");
-  const [isSendButtonDisabled, setIsSendButtonDisabled] = useState(true); // Iniciar deshabilitado
-  const [isExitButtonDisabled, setIsExitButtonDisabled] = useState(true);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
 
-  // Función para actualizar la hora cada minuto
   useEffect(() => {
-    const interval = setInterval(() => {
-      const now = new Date();
-      setFormState({
-        todayDate: now.toISOString().split('T')[0],
-        currentTime: now.toTimeString().split(' ')[0]
-      });
-    }, 60000); // Actualiza cada 60 segundos
-  
-    return () => clearInterval(interval); // Limpiar el intervalo al desmontar el componente
-  }, []);
-  
-  // Verificar la disponibilidad de los botones
-  useEffect(() => {
-    const currentDate = new Date();
-    const currentTimeInMinutes = currentDate.getHours() * 60 + currentDate.getMinutes();
+    const lastAttendanceDate = localStorage.getItem(`lastAttendance_${userId}`);
+    const currentHour = new Date().getHours();
+    const isWithinAllowedTime = currentHour >= 20 && currentHour < 22;
+    setIsButtonDisabled(lastAttendanceDate === formState.todayDate || !isWithinAllowedTime);
+  }, [formState.todayDate, userId]);
 
-    const isWithinSendTime = currentTimeInMinutes >= 7 * 60 && currentTimeInMinutes <= 10 * 60;
-    const hasRecordWithoutExit = attendanceRecords.some(record => record.date === formState.todayDate && !record.exitTime);
-    const isWithinExitTime = currentTimeInMinutes >= 15 * 60 + 30 && currentTimeInMinutes <= 20 * 60;
+  // Definimos waveColors aquí
+  const currentHour = new Date().getHours();
+  const waveColors = currentHour < 8 ? ['#030e2e', '#023a0e', '#05a00d'] : ['#8b0000', '#b22222', '#ff4500'];
 
-    setIsSendButtonDisabled(!isWithinSendTime); // Enviar habilitado entre 7:00 y 10:00
-    setIsExitButtonDisabled(!(isWithinExitTime && hasRecordWithoutExit)); // Salida habilitado entre 15:30 y 20:00
-  }, [attendanceRecords, formState.todayDate]); // Dependencias actualizadas
-  
   const handleAttendance = async () => {
     const todayDate = formState.todayDate;
     const currentTime = formState.currentTime;
@@ -61,50 +48,76 @@ export const Personal = () => {
         date: todayDate,
         time: currentTime,
         status,
-        reason: status === "Tarde" ? reason : "",
-        ip: userIp,
+        reason: reason,
+        ip: userIp        
       };
 
-      const updatedRecords = [...attendanceRecords, record];
-      setAttendanceRecords(updatedRecords);
-      localStorage.setItem(`attendanceRecords_${userId}`, JSON.stringify(updatedRecords));
-      alert("Asistencia registrada correctamente");
+      console.log(record);
+
+      const response = await reportarEntrada(record);
+
+      if (response && response.error) {
+        console.error(response.error);
+        alert("Error al registrar la asistencia: " + response.error.message || response.error);
+      } else {
+        const updatedRecords = [...attendanceRecords, record];
+        setAttendanceRecords(updatedRecords);
+        localStorage.setItem(`attendanceRecords_${userId}`, JSON.stringify(updatedRecords));
+
+        localStorage.setItem(`lastAttendance_${userId}`, todayDate);
+        setIsButtonDisabled(true);
+        alert("Asistencia registrada correctamente");
+      }
     } catch (error) {
-      console.error("Error al registrar asistencia:", error);
-      alert("Hubo un error al registrar la asistencia");
+      console.error("Error al registrar la asistencia:", error);
+      alert("Error al registrar la asistencia: " + error.message);
     } finally {
       setShowPopup(false);
       setReason("");
     }
   };
 
-  const handleMarkExit = () => {
-    const exitTime = new Date().toTimeString().split(' ')[0];
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFormState({
+        todayDate: new Date().toISOString().split('T')[0],
+        currentTime: new Date().toTimeString().split(' ')[0]
+      });
+    }, 1000);
 
-    const updatedRecords = attendanceRecords.map(record =>
-      record.date === formState.todayDate && !record.exitTime
-        ? { ...record, exitTime }
-        : record
-    );
-
-    setAttendanceRecords(updatedRecords);
-    localStorage.setItem(`attendanceRecords_${userId}`, JSON.stringify(updatedRecords));
-
-    alert("Hora de salida registrada correctamente");
-  };
+    return () => clearInterval(interval);
+  }, []);
 
   const handleShowPopup = () => {
-    setShowPopup(true); // Muestra el popup si es necesario
+    setShowPopup(true);
+  };
+
+  const handleConfirmAttendance = () => {
+    setShowPopup(false);
+    handleAttendance();
+  };
+
+  const handleCancelAttendance = () => {
+    setShowPopup(false);
+    setReason("");
   };
 
   return (
     <div className="personal">
       <Navbar user={user} />
+      
+      {/* Agregamos el Header aquí */}
       <Header />
+
       <div className="posts-personal">
         {user ? (
           <div className="e-card playing">
-            <div className="content-user">
+            <div className="image"></div>
+            <div className="wave" style={{ background: `linear-gradient(744deg, ${waveColors[0]}, ${waveColors[1]} 60%, ${waveColors[2]})` }}></div>
+            <div className="wave" style={{ background: `linear-gradient(744deg, ${waveColors[0]}, ${waveColors[1]} 60%, ${waveColors[2]})`, top: '210px' }}></div>
+            <div className="wave" style={{ background: `linear-gradient(744deg, ${waveColors[0]}, ${waveColors[1]} 60%, ${waveColors[2]})`, top: '420px' }}></div>
+
+            <div className='content-user'>
               <div className="infotop">
                 <img
                   src={user.profilePicture || defaultAvatar}
@@ -117,31 +130,46 @@ export const Personal = () => {
                 </div>
               </div>
             </div>
-            <button onClick={handleShowPopup} disabled={isSendButtonDisabled}>
+            <button onClick={handleShowPopup} disabled={isButtonDisabled}>
               <span>Enviar</span>
-            </button>
-            <button 
-              onClick={handleMarkExit} 
-              disabled={isExitButtonDisabled}>
-              Marcar Salida
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 74 74"
+                height="34"
+                width="34"
+              >
+                <circle strokeWidth="3" stroke="black" r="35.5" cy="37" cx="37"></circle>
+                <path
+                  fill="black"
+                  d="M25 35.5C24.1716 35.5 23.5 36.1716 23.5 37C23.5 37.8284 24.1716 38.5 25 38.5V35.5ZM49.0607 38.0607C49.6464 37.4749 49.6464 36.5251 49.0607 35.9393L39.5147 26.3934C38.9289 25.8076 37.9792 25.8076 37.3934 26.3934C36.8076 26.9792 36.8076 27.9289 37.3934 28.5147L45.8787 37L37.3934 45.4853C36.8076 46.0711 36.8076 47.0208 37.3934 47.6066C37.9792 48.1924 38.9289 48.1924 39.5147 47.6066L49.0607 38.0607ZM25 38.5L48 38.5V35.5L25 35.5V38.5Z"
+                ></path>
+              </svg>
             </button>
 
-            <div className="attendance-records">
-              {attendanceRecords.map((record, index) => (
-                <div key={index} className="attendance-record">
-                  <div>Fecha: {record.date}</div>
-                  <div>Hora de Entrada: {record.time}</div>
-                  {record.exitTime ? (
-                    <div>Hora de Salida: {record.exitTime}</div>
-                  ) : (
-                    <div>Esperando Hora de Salida...</div>
-                  )}
+            {showPopup && (
+              <div className="popup">
+                <div className="popup-content">
+                  <p>¿Estás seguro de que deseas registrar tu asistencia?</p>
+                  <textarea
+                    placeholder="Escribe aquí la razón de tu asistencia"
+                    value={reason}
+                    onChange={(e) => setReason(e.target.value)}
+                    rows="4"
+                    style={{ width: '100%', marginTop: '10px', padding: '8px' }}
+                    disabled={isOnTime === "A tiempo"}
+                  />
+
+                  <div className="popup-actions">
+                    <button onClick={handleConfirmAttendance}>Sí</button>
+                    <button onClick={handleCancelAttendance}>No</button>
+                  </div>
                 </div>
-              ))}
-            </div>
+              </div>
+            )}
           </div>
         ) : (
-          <p>Usuario no autenticado</p>
+          <p>No hay datos del usuario disponibles.</p>
         )}
       </div>
     </div>

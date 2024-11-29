7844689aa595116036f51d68e86bc678940fc3f5
Palma <angelonovpalma29@gmail.com>
2024-11-29 07:14:03 -0600

    AjusteTextArea24

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { Input } from '../Input'; // Asegúrate de importar el componente Input
+import { Input } from '../Input';
 import './header.css';
 
 export const Header = () => {
   const [formState, setFormState] = useState({
-    todayDate: '',
-    currentTime: '',
+    todayDate: 'Cargando...',
+    currentTime: 'Cargando...',
   });
   const [isLate, setIsLate] = useState(false);
 
   const fetchInternetTime = async () => {
     try {
       const response = await fetch('https://worldtimeapi.org/api/timezone/America/Guatemala');
+      if (!response.ok) throw new Error('Error al obtener los datos de la API');
       const data = await response.json();
       const currentDateTime = new Date(data.datetime);
+
       const hours = currentDateTime.getHours();
       const minutes = currentDateTime.getMinutes();
       const formattedTime = `${hours}:${minutes < 10 ? `0${minutes}` : minutes}`;
@@ -24,21 +26,15 @@ export const Header = () => {
         currentTime: formattedTime,
       });
 
-      setIsLate(hours >= 8); // Actualiza el estado "A tiempo/Tarde"
+      setIsLate(hours >= 8);
     } catch (error) {
-      console.error("Error fetching internet time:", error);
+      console.error('Error fetching internet time:', error);
     }
   };
 
   useEffect(() => {
     fetchInternetTime();
-    const interval = setInterval(fetchInternetTime, 60000); // Actualiza cada minuto desde la API
-    return () => clearInterval(interval); // Limpia el intervalo al desmontar el componente
   }, []);
-
-  const handleInputValueChange = (value, field) => {
-    // En este caso, no es necesario manejar cambios porque los campos son solo de lectura
-  };
 
   return (
     <div className="header-container">
@@ -47,17 +43,15 @@ export const Header = () => {
           field="fecha"
           label="Fecha"
           value={formState.todayDate}
-          onChangeHandler={handleInputValueChange}
           type="text"
-          disabled={true}
+          disabled
         />
         <Input
           field="hora"
           label="Hora"
           value={formState.currentTime}
-          onChangeHandler={handleInputValueChange}
           type="text"
-          disabled={true}
+          disabled
         />
       </div>
       <div className="status-container">

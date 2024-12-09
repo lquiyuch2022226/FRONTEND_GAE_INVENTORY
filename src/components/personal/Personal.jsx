cc2589e7ebee2dc0841edae424a4d9a85e2ebd28
unknown <Administrador@PC-ELECTRO-026.DIGITALGOB.LOCAL>
2024-12-09 12:23:57 -0600

    pruebas6

--- src/components/personal/Personal.jsx
+++ src/components/personal/Personal.jsx
@@ -1,51 +1,46 @@
-import React, { useState, useEffect } from 'react'; // Importa React y hooks como useState y useEffect
-import { Navbar } from '../Navbar.jsx'; // Importa el componente Navbar
-import { useNavigate } from "react-router-dom"; // Hook para navegar entre rutas
-import { reportarEntrada } from '../../services/api.jsx'; // Importa la función para reportar la entrada del usuario
-import './personal.css'; // Importa los estilos CSS para este componente
-import { Header } from '../header/Header.jsx'; // Importa el componente Header
-import defaultAvatar from '../../assets/img/palmamorro.jpg'; // Avatar predeterminado si el usuario no tiene uno
-import axios from 'axios'; // Importa Axios para hacer peticiones HTTP
-import { Modal } from '../modal/Modal.jsx'; // Importa el componente Modal para mostrar el formulario de llegada tarde
+import React, { useState, useEffect } from 'react'; 
+import { Navbar } from '../Navbar.jsx'; 
+import { useNavigate } from "react-router-dom"; 
+import { reportarEntrada } from '../../services/api.jsx'; 
+import './personal.css'; 
+import { Header } from '../header/Header.jsx'; 
+import defaultAvatar from '../../assets/img/palmamorro.jpg'; 
+import axios from 'axios'; 
+import { Modal } from '../modal/Modal.jsx'; 
 
 export const Personal = () => {
-  // Se obtiene el usuario desde localStorage o se usa un valor por defecto
-  const user = JSON.parse(localStorage.getItem('datosUsuario')) || {};
-  const userId = user.account?.homeAccountId || "Invitado"; // Obtiene el ID de usuario
-  const userName = user.account?.name || "Invitado"; // Obtiene el nombre del usuario
-  const currentHour = new Date().getHours(); // Obtiene la hora actual
-  // Establece los colores del fondo dependiendo de la hora (antes o después de las 8am)
+  const user = JSON.parse(localStorage.getItem('datosUsuario')) || {}; 
+  const userId = user.account?.homeAccountId || "Invitado"; 
+  const userName = user.account?.name || "Invitado"; 
+  const currentHour = new Date().getHours(); 
   const waveColors = currentHour < 8 ? ['#030e2e', '#023a0e', '#05a00d'] : ['#8b0000', '#b22222', '#ff4500'];
 
-  // Definir el estado de los registros de asistencia y la información del formulario
-  const [attendanceRecords, setAttendanceRecords] = useState([]);
+  const [attendanceRecords, setAttendanceRecords] = useState([]); 
   const [formState, setFormState] = useState({
-    todayDate: new Date().toISOString().split('T')[0], // Fecha actual
-    currentTime: new Date().toTimeString().split(' ')[0], // Hora actual
-  });
-  const [showModal, setShowModal] = useState(false); // Estado para mostrar el modal de llegada tarde
-  const [reason, setReason] = useState(""); // Razón de la llegada tarde
-  const [isButtonDisabled, setIsButtonDisabled] = useState(false); // Estado que controla si el botón de asistencia está habilitado
+    todayDate: new Date().toISOString().split('T')[0], 
+    currentTime: new Date().toTimeString().split(' ')[0],
+  }); 
+  const [showModal, setShowModal] = useState(false); 
+  const [reason, setReason] = useState(""); 
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false); 
 
-  // Efecto que sincroniza la hora y fecha actuales cada segundo
   useEffect(() => {
     const timer = setInterval(() => {
       const now = new Date();
       setFormState({
-        todayDate: now.toISOString().split('T')[0], // Actualiza la fecha
-        currentTime: now.toTimeString().split(' ')[0], // Actualiza la hora
+        todayDate: now.toISOString().split('T')[0], 
+        currentTime: now.toTimeString().split(' ')[0], 
       });
     }, 1000);
 
-    return () => clearInterval(timer); // Limpiar el intervalo cuando el componente se desmonta
+    return () => clearInterval(timer);
   }, []);
 
-  // Verifica si el botón debe estar deshabilitado (si ya registró asistencia o si está fuera del rango horario permitido)
   useEffect(() => {
     const lastAttendanceDate = localStorage.getItem(`lastAttendanceDate_${userId}`);
     const [currentHour] = formState.currentTime.split(':').map(Number);
-    const isWithinAllowedTime = currentHour >= 11 && currentHour < 14; // Asistencia solo entre las 6am y las 10am
-    setIsButtonDisabled(lastAttendanceDate === formState.todayDate || !isWithinAllowedTime); // Deshabilita el botón si ya registró o si está fuera del tiempo permitido
+    const isWithinAllowedTime = currentHour >= 11 && currentHour < 14; 
+    setIsButtonDisabled(lastAttendanceDate === formState.todayDate || !isWithinAllowedTime);
   }, [formState.todayDate, formState.currentTime, userId]);
 
   // Función para obtener la IP pública del usuario
@@ -77,7 +72,6 @@ export const Personal = () => {
       const response = await reportarEntrada(record); // Llama a la función para reportar la entrada al servidor
       console.log("Respuesta del API:", response);
 
-      // Verifica si el mensaje de respuesta es exitoso
       if (response.msg === 'Registros de asistencia almacenados correctamente') {
         const updatedRecords = [...attendanceRecords, record]; // Actualiza el estado con el nuevo registro
         setAttendanceRecords(updatedRecords);
@@ -100,7 +94,7 @@ export const Personal = () => {
 
   // Maneja el clic en el botón de asistencia
   const handleButtonClick = () => {
-    const [currentHour, currentMinute] = formState.currentTime.split(':').map(Number); // Obtiene la hora y minuto actuales
+    const [currentHour, currentMinute] = formState.currentTime.split(':').map(Number); 
     if (currentHour >= 8 || (currentHour === 8 && currentMinute > 0)) {
       setShowModal(true); // Si es tarde, muestra el modal para ingresar la razón
     } else {
@@ -110,32 +104,29 @@ export const Personal = () => {
 
   return (
     <div className="personal">
-      <Navbar user={user} /> {/* Muestra el componente Navbar con la información del usuario */}
-      <Header currentDate={formState.todayDate} /> {/* Muestra el componente Header con la fecha actual */}
+      <Navbar user={user} /> 
+      <Header currentDate={formState.todayDate} /> 
       <div className="posts-personal">
         <div className="e-card">
-          {/* Animaciones de fondo en función de la hora del día */}
           <div className="wave" style={{ background: `linear-gradient(744deg, ${waveColors[0]}, ${waveColors[1]} 60%, ${waveColors[2]})` }}></div>
           <div className="wave" style={{ background: `linear-gradient(744deg, ${waveColors[0]}, ${waveColors[1]} 60%, ${waveColors[2]})`, top: '210px' }}></div>
           <div className="wave" style={{ background: `linear-gradient(744deg, ${waveColors[0]}, ${waveColors[1]} 60%, ${waveColors[2]})`, top: '420px' }}></div>
 
           <div className="content-user">
-            {/* Muestra la imagen de perfil o un avatar por defecto */}
             <img
               src={user.profilePicture || defaultAvatar}
               alt="User Icon"
               className="icon"
             />
             <div className="user-info text-white">
-              <div className="user-name">{userName}</div> {/* Nombre del usuario */}
-              <div className="user-department">{user.account.username}</div> {/* Departamento o nombre de la cuenta */}
+              <div className="user-name">{userName}</div> 
+              <div className="user-department">{user.account.username}</div> 
             </div>
           </div>
 
-          {/* Botón para registrar la asistencia */}
           <button
             onClick={handleButtonClick}
-            disabled={isButtonDisabled} // Deshabilita el botón si no está permitido registrar asistencia
+            disabled={isButtonDisabled} 
           >
             Enviar
             <svg
@@ -145,7 +136,6 @@ export const Personal = () => {
               height="34"
               width="34"
             >
-              {/* Icono del botón */}
               <circle strokeWidth="3" stroke="black" r="35.5" cy="37" cx="37"></circle>
               <path
                 fill="black"
@@ -155,16 +145,15 @@ export const Personal = () => {
           </button>
         </div>
 
-        {/* Modal que se muestra cuando el usuario llega tarde */}
         <Modal show={showModal} onClose={() => setShowModal(false)}>
           <div className="modal-content">
             <p>¿Por qué llegaste tarde?</p>
             <textarea
               placeholder="Escribe aquí la razón de tu asistencia"
               value={reason}
-              onChange={(e) => setReason(e.target.value)} // Actualiza la razón cuando el usuario escribe
+              onChange={(e) => setReason(e.target.value)} 
             />
-            <button onClick={() => handleAttendance(true)}>Confirmar</button> {/* Llama a handleAttendance con 'true' para indicar que el usuario llegó tarde */}
+            <button onClick={() => handleAttendance(true)}>Confirmar</button>
           </div>
         </Modal>
       </div>
